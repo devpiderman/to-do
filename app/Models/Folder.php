6b975f16f64acb68ee60ade2204feb00919d6eea
Amirreza Jananfar <amirrezajananfar@gmail.com>
2024-09-08 14:33:16 +0330

    Folder Migration & Model Created With Its Relations

---
+++ app/Models/Folder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class Folder extends Model
+{
+    use HasApiTokens, HasFactory;
+
+    protected $fillable = ['title'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function folders()
+    {
+        return $this->hasMany(Folder::class);
+    }
 }
