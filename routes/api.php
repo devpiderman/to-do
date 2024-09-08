e56c49379ab3f30ee9dbde494210d2f63d0f3bcc
Amirreza Jananfar <amirrezajananfar@gmail.com>
2024-09-08 16:25:38 +0330

    Folders Routes Defined

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\FolderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(FolderController::class)->prefix('/folders')->middleware(['auth:sanctum'])->group(function () {
+    Route::get('/', 'index')->name('folders.index');
+    Route::post('/', 'store')->name('folders.store');
+    Route::get('/{folder}', 'show')->name('folders.show');
+    Route::put('/{folder}', 'update')->name('folders.update');
+    Route::delete('/{folder}', 'destroy')->name('folders.destory');
 });
