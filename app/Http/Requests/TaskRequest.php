79df0483eec04fe45c7de297ff8dd422f8e08eff
Amirreza Jananfar <amirrezajananfar@gmail.com>
2024-09-10 09:28:33 +0330

    Task Form Requested Created & Configured

---
+++ app/Http/Requests/TaskRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\TaskStatus;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class TaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string'],
+            'description' => ['nullable', 'string'],
+            'status' => ['required', new Enum(TaskStatus::class)],
+            'folder_id' => ['required', 'exists:folders,id']
+        ];
+    }
+}
