8e2afaed8cf9e0f64fbe142c9c926c6e98371210
Amirreza Jananfar <amirrezajananfar@gmail.com>
2024-09-13 00:05:58 +0330

    CSRF Token Exception Cleared

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,10 +20,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
-        $middleware->validateCsrfTokens(except: [
-            '/*'
-        ]);
-
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
