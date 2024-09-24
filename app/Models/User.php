919f53e6a9c750795ba93f77b7d0777b5336b1f8
Amirreza Jananfar <amirrezajananfar@gmail.com>
2024-09-24 16:20:17 +0330

    Relation Between User & Log Models Created

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Task::class);
     }
+
+    public function logs()
+    {
+        return $this->hasMany(Log::class);
+    }
 }
